cadc31fc71cde760bdda6a959576803b73f1b6e3
hurrii <hurrii.ftw@gmail.com>
2019-04-21 04:01:30 +0300

    Menu modal window changes bg on scroll

--- app/assets/js/menu__button.js
+++ app/assets/js/menu__button.js
@@ -3,6 +3,43 @@ var menuWindow = document.getElementById('menuWindow');
 
 
 
+// Menu slides to the right
 menuButton.onclick = function showMenuWindow() {
     menuWindow.classList.toggle('menu__container_active');
 }
+
+// Menu window background color changes on scroll
+//var blocksScrollHeight = ;
+//alert((blockAbout.scrollHeight + blockMyWorks.scrollHeight));
+
+
+window.onscroll = function changeBackgroundOnScroll() {
+    // Page block variables
+    var blockAbout = document.getElementById('blockAbout').scrollHeight;
+    var blockMyWorks = document.getElementById('blockMyWorks').scrollHeight;
+    var blockSkills = document.getElementById('blockSkills').scrollHeight;
+
+    //
+    
+
+    //  Scroll position
+    var scrollPosition = document.documentElement.scrollTop;
+
+
+
+    if (scrollPosition >= (blockAbout + blockMyWorks + blockSkills - 50)) {
+        menuWindow.classList.add('bg-silver');
+    }
+
+    else if (scrollPosition >= (blockAbout + blockMyWorks - 50)) {
+        menuWindow.classList.add('bg-navyblue');
+    }
+
+
+    else {
+        menuWindow.classList.remove('bg-navyblue');
+        menuWindow.classList.remove('bg-silver');
+    }
+}
+
+//         menuWindow.classList.toggle('bg-navyblue')
